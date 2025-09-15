2cce1dc7075e0cc3aeca3acc5bf4db6dd2ce7dfe
rociocjz <rociocojulum@gmail.com>
2025-09-14 23:01:59 -0600

    Update #proyecto.py
    se creo clase estudiante

--- #proyecto.py
+++ #proyecto.py
@@ -12,3 +12,22 @@ class Usuario(ABC):
     @property
     def nombre(self):
         return self._nombre
+    
+class Estudiante(Usuario):
+    def __init__(self, carnet, nombre):
+        super().__init__(carnet, nombre)
+        self._cursos = set()
+
+    
+    def asigancion(self, nombrecurso ):
+
+        while True:
+            if nombrecurso in self._cursos:
+                print(f"El curso {nombrecurso} ya está asignado para el/ la estudiante  {self.nombre}.")
+                nombrecurso = input("Ingrese un nuevo nombre de curso: ")
+            else:
+                self._cursos.add(nombrecurso)
+                print(f"Curso {nombrecurso} asignado exitosamente al estudiante {self.nombre}.")
+                break
+
+    
